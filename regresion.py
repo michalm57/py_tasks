1727e3ef2dc1e286cd050f54abd53fe504d5d389
michalm57 <mroczekmichal57@gmail.com>
2024-04-15 15:25:33 +0200

    #5 Regresion

---
+++ regresion.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import quandl
+import math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+
+df = quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+print(df.head())
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
+print(df.head())
+
+x = np.array(df.drop(['label']))
+y = np.array(df['label'])
+
+x = preprocessing.scale(x)
+
+x = x[:-forecast_out+1]
+df.dropna(inplace=True)
+y = np.array(df['label'])
+
+
+print(len(x), len(y))
