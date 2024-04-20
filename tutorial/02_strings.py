3babfe1cc25ee5e5ca6d737f9f679d66f00da5cd
michalm57 <mroczekmichal57@gmail.com>
2024-04-20 13:18:56 +0200

    #9 Strings

---
+++ tutorial/02_strings.py
@@ -0,0 +1,15 @@
+message = "Meet me tonight."
+print(message)
+
+message2 = 'The clock strikes at midnight.'
+print(message2)
+
+message3 = 'I\'m looking for someone.'
+message3 = "I'm looking for someone."
+
+message4 = 'Test "example" test'
+
+movie_quote = """Test test test test 
+et  teteetsa a as das asd a
+asdasdasdas  s d as asw as 
+"""
