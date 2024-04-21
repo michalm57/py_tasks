c243acec40e92e3f3da63b781bf29d06bc04c966
michalm57 <mroczekmichal57@gmail.com>
2024-04-21 13:03:35 +0200

    #10 Numbers

---
+++ tutorial/03_numbers.py
@@ -0,0 +1,18 @@
+# Types of numbers: int, float, complex
+# Whole numbers: int, long
+
+a = 28 
+type(a)
+# <class 'int'>
+
+#Floats
+e = 2.718281828
+type(e)
+#<class 'float'>
+
+#Complex Numbers, #Liczby zespolone
+z = 3 + 5.7j
+type(z)
+z.real # Real part
+z.imag # Imaginary part
+#<class 'complex'>
