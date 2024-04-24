a5d5e0f630fc761642fd6af8cff75e0b0ba2fe43
michalm57 <mroczekmichal57@gmail.com>
2024-04-24 16:51:02 +0200

    #11 Arithmetic

---
+++ tutorial/04_arithmetic.py
@@ -0,0 +1,34 @@
+# Operations: + - * /
+
+x = 28 # int 
+y = 29.0 # float
+
+#Float to int conversion
+int(3.14)
+#3
+
+#Float to complec conversion
+1.732 + 0j
+complex(1.732)
+
+# ERROR e.g: float(1.732 + 0j)
+# Floats are narrower than complex numbers
+# Complex numbers are wider than floats
+
+# Arithmetic Operations
+a = 2 # int
+b = 6.0 # float 
+c = 12 + 0j # complex number
+
+# Addition 
+a + b # int + float
+
+# Subtraction
+b - a # float - int
+
+#Multiplication
+a * 7  # int * int
+
+# Division - always return float
+c / b # complex / float
+16//5 # Return quotient
