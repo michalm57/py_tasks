77cd8929155f9958e8e65e19290a724757176a4a
michalm57 <mroczekmichal57@gmail.com>
2024-04-27 06:40:13 +0200

    #12 Interactive help

---
+++ tutorial/05_interactive_help.py
@@ -0,0 +1,15 @@
+import math
+
+dir() # Short for "directory"
+#['__annotations__', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__']
+
+dir(__builtins__)
+
+help(pow)
+pow(2, 10)
+hex(10) # In python begin with 0x
+
+#List of modules
+help('modules')
+help(math.radians)
+math.radians(180)
