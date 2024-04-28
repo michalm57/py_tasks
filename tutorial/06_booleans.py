496b9bf88271339d5592a3d4b9cfbf625bd67276
michalm57 <mroczekmichal57@gmail.com>
2024-04-28 19:56:16 +0200

    13 # Booleans

---
+++ tutorial/06_booleans.py
@@ -0,0 +1,24 @@
+# Boolean values: True, False
+a = 3
+b = 5
+
+a == b
+#False 
+
+a != b
+#True
+
+a > b
+
+type(True)
+bool(-2) # Returns True
+bool(0) # Return False
+
+str(True)
+str(False)
+
+int(True) #1
+int(False) #0
+
+5 + True # True as 1
+10 * False # False as 0
