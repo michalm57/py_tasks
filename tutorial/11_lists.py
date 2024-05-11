1b5893c56229ce52bab5ee4ec417ddb761e43742
michalm57 <mroczekmichal57@gmail.com>
2024-05-11 08:39:55 +0200

    18 # Lists

---
+++ tutorial/11_lists.py
@@ -0,0 +1,29 @@
+example = list()
+example = []
+
+primes = [2, 3, 5, 7, 11, 13]
+primes.append(17)
+primes.append(19)
+#Last item in list is -1, next is -2 etc.
+
+
+# Includes the value of begining but exluding end values.
+primes[2:5]
+# Result is 5,7,11
+
+#List may containt all types of data
+example = [128, True, "Alpha", 1.732, [64, False]]
+
+rolls = [4, 7, 2, 7, 12, 4, 7]
+
+numbers = [1, 2, 3]
+letters = ['a', 'b', 'c']
+
+# Order matters
+sum = numbers + letters
+sum2 = letters + numbers
+
+# e.g. List function, reverse list
+numbers.reverse()
+
+
