f90653f950ff0dcbc3b8ec7301041e6a3d8f099b
michalm57 <mroczekmichal57@gmail.com>
2024-08-01 20:46:34 +0200

    #29 Pydoc

---
+++ tutorial/22_pydoc.py
@@ -0,0 +1,4 @@
+# Searching pyDoc via module name: python -m pydoc math
+# Searching pyDoc via keyword: python -m pydoc -k ftp
+# Display pyDoc in browser: sudo python3 -m pydoc -p 314
+# python -m pydoc -w json Generate html file containing description of JSON
